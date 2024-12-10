c7558ac47c2a58c888993f45d59c620de186b581
Anamul Haque <138350161+Anamul9901@users.noreply.github.com>
2024-12-10 11:29:12 +0600

    Setup my file

---
+++ src/app/routes/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import { UserRouter } from "../modules/User/user.router";
+import { AuthRoutes } from "../modules/Auth/auth.routes";
+
+const router = express.Router();
+
+const moduleRoutes = [
+  {
+    path: "/users",
+    route: UserRouter,
+  },
+  {
+    path: "/auth",
+    route: AuthRoutes,
+  },
+];
+
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
+
+export default router;
